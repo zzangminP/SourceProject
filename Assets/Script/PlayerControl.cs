f90c965a07431becac75718585ed7e41f428460d
zzangmin <wjdals1212gh@gmail.com>
2024-08-01 19:50:29 +0900

    [Init] New Project
    New Project

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    private Animator player_ani;
+
+    [SerializeField] private float moveSpeed;
+    private bool isMove;
+
+
+    private void Start()
+    {
+        player_ani = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float _dirX = Input.GetAxis("Horizontal");
+        float _dirZ = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(_dirX, 0, _dirZ);
+
+        isMove = false;
+
+
+        if (direction != Vector3.zero)
+        {
+            isMove = true;
+            this.transform.Translate(direction.normalized * moveSpeed * Time.deltaTime);
+          
+
+        }
+
+        player_ani.SetBool("Move", isMove);
+        player_ani.SetFloat("DirX", direction.x);
+        player_ani.SetFloat("DirZ", direction.z);
+    }
+}
